b32fc144ad81b8148a409995180d7442526081a3
osinoko <k023g0114@g.neec.ac.jp>
2024-04-19 21:33:34 +0900

    各機能の実装

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Audio.h"
+#include "DebugCamera.h"
 #include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
@@ -43,6 +44,33 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	// サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+
+	// 音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+
+	// スプライト
+	Sprite* sprite_ = nullptr;
+
+	// 3Dモデル
+	Model* model_ = nullptr;
+
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	// ImGuiで値を入力する変数
+	float inputFloat3[3] = {0, 0, 0};
+
+	// デバッグカメラ
+	DebugCamera* debugCamera_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
